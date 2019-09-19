1d1cdbce57b0c5d8fa027c6a0f2889aca6c9f5ac
Razvan Anisia <razvan.anisia@elegantdigital.co.uk>
2019-09-19 11:18:09 +0100

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
-import React, { useState, useCallback } from 'react';
-import { Hello } from './Hello';
-const App = () => {
-  const [count, setCount] = useState(0);
+import React, { useState, useReducer } from 'react';
 
-  const increment = useCallback(() => {
-    setCount(c => c + 1);
-  }, [setCount]);
+const ADD_TODO = 'ADD_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
+
+function reducer(state, action) {
+  //react the action and change the state accordingly
+  switch (action.type) {
+    case ADD_TODO:
+      return {
+        todoCount: state.todoCount + 1,
+        todos: [...state.todos, { text: action.text, completed: false }]
+      };
+    case TOGGLE_TODO:
+      return {
+        todos: state.todos.map((todo, id) =>
+          id === action.id ? { ...todo, completed: !todo.completed } : todo
+        ),
+        todoCount: state.todoCount
+      };
+    default:
+      return state;
+  }
+}
+const App = () => {
+  const [{ todos, todoCount }, dispatch] = useReducer(reducer, {
+    todos: [],
+    todoCount: 0
+  });
+  const [text, setText] = useState('');
   return (
     <div>
-      <Hello increment={increment} />
-      <div>{count}</div>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          dispatch({ type: ADD_TODO, text });
+          setText('');
+        }}
+      >
+        <input value={text} onChange={e => setText(e.target.value)}></input>
+      </form>
+      {todos.map((t, i) => (
+        <li
+          style={{ opacity: t.completed ? '0.5' : '1' }}
+          key={i}
+          onClick={() => dispatch({ type: TOGGLE_TODO, id: i })}
+        >
+          {t.text}
+        </li>
+      ))}
+      {todoCount}
     </div>
   );
 };
